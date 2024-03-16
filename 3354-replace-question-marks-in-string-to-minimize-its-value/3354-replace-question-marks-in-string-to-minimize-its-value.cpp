c371c46018394505fbebaebe6ffe142a559b99da
Vraj Shah <svraj073@gmail.com>
2024-03-17 00:11:20 +0530

    Time: 396 ms (16.67%), Space: 26 MB (66.67%) - LeetHub

--- 3354-replace-question-marks-in-string-to-minimize-its-value/3354-replace-question-marks-in-string-to-minimize-its-value.cpp
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/3354-replace-question-marks-in-string-to-minimize-its-value.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     string minimizeStringValue(string s) {
-        unordered_map<char,int>mp;
-        string ans,temp;
+        unordered_map<char, int> mp(0);
+        string ans, temp;
 
-        for(auto &c:s){
-            if(c!='?'){
+        for (auto& c : s) {
+            if (c != '?') {
                 mp[c]++;
-            }
-            else continue;
+            } else
+                continue;
         }
 
-        for(auto &c:s){
+        for (auto& c : s) {
             int cost = INT_MAX;
-            if(c=='?'){
-                for(char ch = 'a';ch<='z';ch++){
-                    cost = min(cost,mp[ch]);
-                    // cout<<cost<<endl;
+            if (c == '?') {
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    cost = min(cost, mp[ch]);
                 }
-                for(char ch = 'a';ch<='z';ch++){
-                    if(mp[ch]==cost){
-                    temp.push_back(ch);
-                    mp[ch]++;
-                    break;
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    if (mp[ch] == cost) {
+                        temp.push_back(ch);
+                        mp[ch]++;
+                        break;
                     }
                 }
             }
         }
-        sort(temp.begin(),temp.end());
+        sort(temp.begin(), temp.end());
         int i = 0;
-        for(auto &c:s){
-            if(c!='?')ans.push_back(c);
-            else{
-            ans.push_back(temp[i]);
-            i++;    
-            } 
+        for (auto& c : s) {
+            if (c != '?')
+                ans.push_back(c);
+            else {
+                ans.push_back(temp[i]);
+                i++;
+            }
         }
         return ans;
     }
