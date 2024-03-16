dfa95ca87e03957a5ffb5d86fd63d05948678a6b
Vraj Shah <svraj073@gmail.com>
2024-03-17 00:09:57 +0530

    Time: 397 ms (16.67%), Space: 25.7 MB (66.67%) - LeetHub

---
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/3354-replace-question-marks-in-string-to-minimize-its-value.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minimizeStringValue(string s) {
+        unordered_map<char,int>mp;
+        string ans,temp;
+
+        for(auto &c:s){
+            if(c!='?'){
+                mp[c]++;
+            }
+            else continue;
+        }
+
+        for(auto &c:s){
+            int cost = INT_MAX;
+            if(c=='?'){
+                for(char ch = 'a';ch<='z';ch++){
+                    cost = min(cost,mp[ch]);
+                    // cout<<cost<<endl;
+                }
+                for(char ch = 'a';ch<='z';ch++){
+                    if(mp[ch]==cost){
+                    temp.push_back(ch);
+                    mp[ch]++;
+                    break;
+                    }
+                }
+            }
+        }
+        sort(temp.begin(),temp.end());
+        int i = 0;
+        for(auto &c:s){
+            if(c!='?')ans.push_back(c);
+            else{
+            ans.push_back(temp[i]);
+            i++;    
+            } 
+        }
+        return ans;
+    }
+};
