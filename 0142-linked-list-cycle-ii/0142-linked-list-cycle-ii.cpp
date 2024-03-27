9bc5f81cbbc9f12722ecc09254e072e4859bb0d7
Vraj Shah <svraj073@gmail.com>
2024-03-28 01:26:00 +0530

    Time: 2 ms (98.09%), Space: 10.2 MB (42.67%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,13 +6,13 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while (fast->next != NULL) {
-            fast = fast->next;
-            if (fast->next != NULL) {
-                fast = fast->next;
-            }
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            // if (fast->next != NULL) {
+            //     fast = fast->next;
+            // }
             slow = slow->next;
-            if (fast == slow && slow != NULL) {
+            if (fast == slow  && slow != NULL) {
                 slow = head;
                 while (fast != slow) {
                     fast = fast->next;
