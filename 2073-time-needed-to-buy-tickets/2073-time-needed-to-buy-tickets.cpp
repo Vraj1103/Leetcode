19a744c794f19c7355e0ed64c69087a886d2ea6b
Vraj Shah <svraj073@gmail.com>
2024-04-09 15:09:42 +0530

    Time: 4 ms (41.19%), Space: 9.3 MB (67.56%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        while (tickets[k]) {
+            for (int i = 0; i < tickets.size(); i++) {
+                if (tickets[i] != 0) {
+                    tickets[i]--;
+                    time++;
+                }
+                if (tickets[k] == 0)
+                    break;
+            }
+        }
+        return time;
+    }
+};
